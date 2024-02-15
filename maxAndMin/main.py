d9f4d0355d9a46824798a02fe4dddba25014e9b9
marwan <marwanmj00@gmail.com>
2024-02-16 01:00:27 +0300

    added new challenge

---
+++ maxAndMin/main.py
@@ -0,0 +1,17 @@
+def maxnum(list):
+    most = list [0] 
+    for i in list:
+       if i > most:
+        most = i
+    print (most)
+maxnum([-9 , -10 , -32 , -43 ,-40 ,-2, -7])
+
+
+def minnum(list):
+    least = list [0] 
+    for i in list:
+       if i < least:
+        least = i
+    print (least)
+minnum([2, -3, 30, -90, -100 ,0])    
+
--- reverseStr/main.py
+++ reverseStr/main.py
@@ -5,4 +5,15 @@ def reverse_str():
     list = ["i", "will", "reverse", "this"]
     list.reverse()
     print (list)
-reverse_str()
+# reverse_str()
+
+# list = [1,4,7,1,2,6,8]
+
+# for batates in range(len(list)): 
+#     if list[batates] > 5:
+#         print("your total is" ,list[batates] * 5 -5, 'dollar' )
+#     else:
+#         print("your total is" ,list[batates] * 5 , 'dollar' )
+
+
+
