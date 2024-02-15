210bd9976281995700d43b2f1f5554f4958f417e
marwan <marwanmj00@gmail.com>
2024-02-15 16:03:31 +0300

    first commit

---
+++ reverseStr/main.py
@@ -0,0 +1,8 @@
+def reverse_str():
+    #make a list out of word or str
+    #reverse it 
+    #then print it
+    list = ["i", "will", "reverse", "this"]
+    list.reverse()
+    print (list)
+reverse_str()
