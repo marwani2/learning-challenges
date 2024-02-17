a0371950c9d3359a60e87a8d0851ab1683ead664
marwan <marwanmj00@gmail.com>
2024-02-17 13:58:41 +0300

    changed reverse_str to reverse letters

--- reverseStr/main.py
+++ reverseStr/main.py
@@ -1,19 +1,5 @@
-def reverse_str():
-    #make a list out of word or str
-    #reverse it 
-    #then print it
-    list = ["i", "will", "reverse", "this"]
-    list.reverse()
-    print (list)
-# reverse_str()
-
-# list = [1,4,7,1,2,6,8]
-
-# for batates in range(len(list)): 
-#     if list[batates] > 5:
-#         print("your total is" ,list[batates] * 5 -5, 'dollar' )
-#     else:
-#         print("your total is" ,list[batates] * 5 , 'dollar' )
-
-
-
+def reverse_str(str):
+    a = str [::-1]
+    return a
+#example
+print (reverse_str('ih'))
